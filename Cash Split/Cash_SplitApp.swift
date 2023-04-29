0076d40db418f800f60ffcff438f50a9a7ffc4c4
pravalgautam <pravalgautam79@gmail.com>
2023-04-29 17:55:35 +0530

    Initial Commit

---
+++ Cash Split/Cash_SplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cash_SplitApp.swift
+//  Cash Split
+//
+//  Created by Praval Gautam on 29/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Cash_SplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
