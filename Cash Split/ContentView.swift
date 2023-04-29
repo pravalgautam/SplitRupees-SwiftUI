8feef978afc359c8e6d732d7ad4938fa15f89116
pravalgautam <pravalgautam79@gmail.com>
2023-04-30 01:28:16 +0530

    ui app to split money between people

--- Cash Split/ContentView.swift
+++ Cash Split/ContentView.swift
@@ -8,14 +8,83 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var  spendAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    
+    let tipPercentages = [10, 15, 20, 25, 0 ]
+    @FocusState private var amountisfocused: Bool
+    
+    
+    var totalPerPerson :Double{
+        let tipselection = Double(tipPercentage)
+        let peopleCount = Double(numberOfPeople+2)
+        let tipValue = spendAmount / 100 * tipselection
+        let  total =  spendAmount+tipValue
+        let  amountperperson = total / peopleCount
+        
+        return amountperperson
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            Form{
+                
+                Section{
+
+                    TextField("Amount", value: $spendAmount,format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($amountisfocused)
+                    
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2..<100){
+                            Text("\($0) people")
+                        }
+                    }
+                }
+            header: {
+                    Text("How much you spend?")
+                }
+                
+                
+                
+                
+                
+                Section {
+                                Picker("Tip percentage", selection: $tipPercentage) {
+                                    ForEach(tipPercentages, id: \.self) {
+                                        Text($0, format: .percent)
+                                    }
+                                }
+                    
+                                .pickerStyle(.segmented)
+                    
+                            } header: {
+                                Text("How much tip do you want to leave?")
+                            }
+                            
+                
+                
+                
+                Section{
+                    Text(totalPerPerson,format: .currency(code: "INR"))
+                }
+                
+            }
+            .navigationTitle("Split Your Money")
+            .toolbar{
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done"){
+                        amountisfocused = false
+                    }
+                    
+                }
+                
+            }
+        
         }
-        .padding()
     }
 }
 
